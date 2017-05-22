a5b7216a52024414544d499f718cb84bfe5e01d7
evgeni <evgeni.petrov@gmail.com>
2017-05-22 14:04:17 +0300

    Logins rearrangement.

--- 20347A/20347A-LON-CL1.ps1
+++ 20347A/20347A-LON-CL1.ps1
@@ -14,21 +14,15 @@ Enable-LTRunEverythingAsAdministrator
 $currentScript = Get-LTCurrentFile
 Set-LTRunOnceScript -LiteralPath $currentScript.FullName
 
-if($env:COMPUTERNAME -eq $env:USERDOMAIN){
-    Set-LTAutologon -Username $env:USERNAME -Password $commonPassword
-} else {
-    Set-LTAutologon -Username $adminUsername -Password $adminPassword
-}
-
+Set-LTAutologon -Username $env:USERNAME -Password $commonPassword
 Rename-LTComputer -NewName $computerName
 
 $secAdminPassword = ConvertTo-SecureString $adminPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($adminUsername, $secAdminPassword)
+Set-LTAutologon -Username $adminUsername -Password $adminPassword
 Join-LTDomainComputer -DomainName $domainName -Credential $credential
 
 foreach ($localAdmin in $localAdmins)
 {
   Add-LocalGroupMember -Group Administrators -Member $localAdmin
 }
-
- 
