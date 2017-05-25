a1a0c5d0031c92cd20bb2e48cbbd7e6c2a44978c
evgeni <evgeni.petrov@gmail.com>
2017-05-25 02:17:36 -0700

    Simplified DC1 setup

--- 20347A/20347A-LON-DC1.ps1
+++ 20347A/20347A-LON-DC1.ps1
@@ -9,21 +9,21 @@ $domainName = 'adatum.com'
 $domainUsers = @()
 $domainUsers += @{'SamAccountName'='holly';'DisplayName'='Holly Dickson';'Upn'='holly.dickson@adatum.com';}
 #
-$currentScript = Get-LTCurrentFile
-Set-LTRunOnceScript -LiteralPath $currentScript.FullName
 
-Set-LTAutologon -Username $adminUsername -Password $adminPassword
-
+# 1. Rename Computer
 Rename-LTComputer -NewName $computerName
 
+# 2. Create Domain
 $secAdminPassword = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force
 Add-LTDomainContoller -DomainName $domainName -SafeModeAdministratorPassword $secAdminPassword
 
+# 3. Add Domain Users
 foreach($domainUser in $domainUsers){
     Add-LTUser -SamAccountName $domainUser.SamAccountName -DisplayName $domainUser.DisplayName -Upn $domainUser.Upn -Password $commonPassword
 }
 
 
+# Unload module and cleanup.
 Remove-Module PowerShell.LabTools
 
 
